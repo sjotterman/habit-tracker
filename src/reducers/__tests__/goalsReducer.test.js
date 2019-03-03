ca5d4af821b4053c78d3b5d6ee4751a57125144d
Samuel Otterman <sjotterman@gmail.com>
2019-03-03 14:22:40 -0500

    goal reducers handle dates completed

--- src/reducers/__tests__/goalsReducer.test.js
+++ src/reducers/__tests__/goalsReducer.test.js
@@ -9,7 +9,8 @@ describe('goals reducer', () => {
       });
 
     
-    it('should handle GOAL_TOGGLED to true ', () => {
+    it('should handle GOAL_TOGGLED to true on new date ', () => {
+      const testDate = '2019-02-05';
       const initialState = 
          [
             {
@@ -17,7 +18,12 @@ describe('goals reducer', () => {
                 name: 'Brush my teeth',
                 streak: 6,
                 done: false,
-                last_done: 'two days ago'
+                dates_done: [
+                  '2019-02-01',
+                  '2019-02-02',
+                  '2019-02-03',
+                  '2019-02-04'
+                ]
             },
         ];
       const nextState = 
@@ -27,12 +33,62 @@ describe('goals reducer', () => {
                 name: 'Brush my teeth',
                 streak: 6,
                 done: true,
-                last_done: 'two days ago'
+                dates_done: [
+                  '2019-02-01',
+                  '2019-02-02',
+                  '2019-02-03',
+                  '2019-02-04',
+                  testDate
+                ]
             },
         ];
       const action = {
         type: actions.GOAL_TOGGLED,
         goalId: 'brush-my-teeth',
+        date: testDate
+      };
+        expect(goalsReducer(initialState, action)).toEqual(
+          nextState
+          )
+      });
+
+    it('should handle GOAL_TOGGLED to true on older date ', () => {
+      const testDate = '2019-02-04';
+      const initialState = 
+         [
+            {
+                id: 'brush-my-teeth',
+                name: 'Brush my teeth',
+                streak: 6,
+                done: false,
+                dates_done: [
+                  '2019-02-01',
+                  '2019-02-02',
+                  '2019-02-05',
+                  '2019-02-06'
+                ]
+            },
+        ];
+      const nextState = 
+         [
+            {
+                id: 'brush-my-teeth',
+                name: 'Brush my teeth',
+                streak: 6,
+                done: true,
+                dates_done: [
+                  '2019-02-01',
+                  '2019-02-02',
+                  testDate,
+                  '2019-02-05',
+                  '2019-02-06'
+                ]
+            },
+        ];
+      const action = {
+        type: actions.GOAL_TOGGLED,
+        goalId: 'brush-my-teeth',
+        date: testDate
       };
         expect(goalsReducer(initialState, action)).toEqual(
           nextState
@@ -40,29 +96,42 @@ describe('goals reducer', () => {
       });
 
     it('should handle GOAL_TOGGLED to false ', () => {
-      const initialState =
+      const testDate = '2019-02-05';
+      const initialState = 
          [
             {
-                id: 'eat-food',
-                name: 'Eat Food',
+                id: 'brush-my-teeth',
+                name: 'Brush my teeth',
                 streak: 6,
                 done: true,
-                last_done: 'today'
+                dates_done: [
+                  '2019-02-01',
+                  '2019-02-02',
+                  '2019-02-03',
+                  '2019-02-04',
+                  testDate
+                ]
             },
         ];
-      const nextState =
+      const nextState = 
          [
             {
-                id: 'eat-food',
-                name: 'Eat Food',
+                id: 'brush-my-teeth',
+                name: 'Brush my teeth',
                 streak: 6,
                 done: false,
-                last_done: 'today'
+                dates_done: [
+                  '2019-02-01',
+                  '2019-02-02',
+                  '2019-02-03',
+                  '2019-02-04',
+                ]
             },
         ];
       const action = {
         type: actions.GOAL_TOGGLED,
-        goalId: 'eat-food',
+        goalId: 'brush-my-teeth',
+        date: testDate
       };
         expect(goalsReducer(initialState, action)).toEqual(
           nextState
--- src/reducers/goalsReducer.js
+++ src/reducers/goalsReducer.js
@@ -4,13 +4,22 @@ export default function goalsReducer(state = [], action) {
     switch(action.type) {
         case types.GOAL_TOGGLED:
             let updatedGoals = state.map( item => {
-                    if (item.id === action.goalId) {
-                        let newItem = item;
-                        const done = newItem.done ? false : true;
-                        return {...item, done};
+                if (item.id === action.goalId) {
+                    let newItem = item;
+                    const done = newItem.done ? false : true;
+                    let dates_done = newItem.dates_done;
+                    if (done) {
+                        dates_done.push(action.date);
+                        dates_done.sort();
+                    } else {
+                        dates_done = dates_done.filter(date => {
+                            return date !== action.date;
+                        })
                     }
+                    return { ...item, done, dates_done };
+                }
                 return item;
-            })
+            });
             return updatedGoals;
 
         default:
