b1e192c600c5122d9eed9c6827108c7bcd4cb23e
Samuel Otterman <sjotterman@gmail.com>
2019-03-03 13:50:52 -0500

    refactor goalsReducer.test.js

--- src/reducers/__tests__/goalsReducer.test.js
+++ src/reducers/__tests__/goalsReducer.test.js
@@ -1,22 +1,16 @@
 import * as actions from '../../actions/actionTypes';
-import reducer from '../index';
-// import counterReducer from '../counterReducer';
-// import reducer from '../counterReducer';
-import { types } from 'util';
+import goalsReducer from '../goalsReducer';
 
 describe('goals reducer', () => {
-    // it('should return the initial state', () => {
-    //     expect(reducer(undefined, {})).toEqual(
-    //       []
-    //       )
-    //   });
+    it('should return the initial state', () => {
+        expect(goalsReducer(undefined, {})).toEqual(
+          []
+          )
+      });
 
     
     it('should handle GOAL_TOGGLED to true ', () => {
       const initialState = 
-      {
-      counters: [],
-      goals:
          [
             {
                 id: 'brush-my-teeth',
@@ -25,12 +19,8 @@ describe('goals reducer', () => {
                 done: false,
                 last_done: 'two days ago'
             },
-        ]
-      };
+        ];
       const nextState = 
-      {
-      counters: [],
-      goals:
          [
             {
                 id: 'brush-my-teeth',
@@ -39,13 +29,12 @@ describe('goals reducer', () => {
                 done: true,
                 last_done: 'two days ago'
             },
-        ]
-      };
+        ];
       const action = {
         type: actions.GOAL_TOGGLED,
         goalId: 'brush-my-teeth',
       };
-        expect(reducer(initialState, action)).toEqual(
+        expect(goalsReducer(initialState, action)).toEqual(
           nextState
           )
       });
