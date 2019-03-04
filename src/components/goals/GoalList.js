6fc56a020513f6f76c25a3b6f890b9ff25e018ef
Samuel Otterman <sjotterman@gmail.com>
2019-03-03 21:22:23 -0500

    refactor GoalList into separate component

---
+++ src/components/goals/GoalList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import GoalRow from './GoalRow';
+
+const GoalList = (props) => {
+   let { goals, onGoalToggle } = props; 
+    return (
+        <div className="row">
+        <div className="text-light col-sm-4 col-m-4">
+            <table className="table">
+            <thead>
+                    <tr className="text-light">
+                        <th scope="col">Goal</th>
+                        <th scope="col">Best Streak</th>
+                        <th scope="col">Current Streak</th>
+                        <th scope="col">Done?</th>
+                    </tr>
+            </thead>
+            <tbody>
+                {goals.map((item, id) => {
+                    return (
+                        <GoalRow goal={item} key={id} onGoalToggle={() => onGoalToggle(item.id)} />
+                    );
+                })}
+            </tbody>
+            </table>
+        </div>
+    </div>
+    );
+
+}
+
+export default GoalList;
--- src/components/goals/GoalsPage.js
+++ src/components/goals/GoalsPage.js
@@ -2,38 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as goalsActions from '../../actions/goalsActions';
-import GoalRow from './GoalRow';
+import GoalList from './GoalList';
 
 
 class GoalsPage extends React.Component {
 
     render() {
+        let { goals, actions } = this.props;
         return (
             <div>
                 <h1 className="text-light">Goals</h1>
-                <div className="row">
-                    <div className="text-light col-sm-4 col-m-4">
-                        <table className="table">
-                        <thead>
-                                <tr className="text-light">
-                                    <th scope="col">Goal</th>
-                                    <th scope="col">Best Streak</th>
-                                    <th scope="col">Current Streak</th>
-                                    <th scope="col">Done?</th>
-                                </tr>
-                        </thead>
-                        <tbody>
-
-                            {this.props.goals.map((item, id) => {
-                                return (
-                                    <GoalRow goal={item} key={id} onGoalToggle={() => this.props.actions.toggleGoal(item.id)} />
-                                );
-                            })}
-                        </tbody>
-                        </table>
-                    </div>
-                </div>
-                 
+                <GoalList goals={goals} onGoalToggle={actions.toggleGoal} />
             </div>
         )
     }
