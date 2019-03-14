9555e7ea1be43280817364bfc5837fd68cea9c63
Samuel Otterman <sjotterman@gmail.com>
2019-03-13 21:59:17 -0400

    json server up and running

---
+++ buildScripts/generateMockData.js
@@ -0,0 +1,28 @@
+/*
+  This script generates mock data for local development.
+*/
+
+/* eslint-disable no-console */
+
+import jsf from 'json-schema-faker';
+import {schema} from './mockDataSchema';
+import fs from 'fs';
+import chalk from 'chalk';
+
+const fixedUser = {
+  "id" : "12345678",
+  "firstName": "Samuel",
+  "lastName" : "Otterman",
+  "email" : "sam@example.com"
+}
+const data = jsf.generate(schema);
+data["users"].push(fixedUser);
+const json = JSON.stringify(data);
+
+fs.writeFile("./src/api/db.json", json, function(err) {
+  if(err) {
+    return console.log(chalk.red(err));
+  } else {
+    console.log(chalk.green("Mock data generated."));
+  }
+});
