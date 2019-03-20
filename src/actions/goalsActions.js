aff0e66286bd33919dcb8709fdd178214047b12f
Samuel Otterman <sjotterman@gmail.com>
2019-03-19 23:10:03 -0400

    get and create goals use api calls

--- src/actions/goalsActions.js
+++ src/actions/goalsActions.js
@@ -1,48 +1,58 @@
-import * as types from './actionTypes';
-import { currentDateTime }  from '../utils/timeFormatter';
-import goalApi from '../api/mockGoalApi'
-
+import * as types from "./actionTypes";
+import { currentDateTime } from "../utils/timeFormatter";
+import { getAllGoals, saveNewGoal } from "../api/GoalApi";
 
 export function createGoalSuccess(goal) {
-    return { type: types.CREATE_GOAL_SUCCESS, goal};
+  return { type: types.CREATE_GOAL_SUCCESS, goal };
 }
 
 export function updateGoalSuccess(goal) {
-    return { type: types.UPDATE_GOAL_SUCCESS, goal};
+  return { type: types.UPDATE_GOAL_SUCCESS, goal };
 }
 
 export function deleteGoal(goalId) {
-    return { type: types.GOAL_DELETED, goalId };
+  return { type: types.GOAL_DELETED, goalId };
 }
 
 export function toggleGoal(goalId, date) {
-    const currentDate = currentDateTime();
+  const currentDate = currentDateTime();
 
-    date = date || currentDate;
-    return { type: types.GOAL_TOGGLED, goalId, date};
+  date = date || currentDate;
+  return { type: types.GOAL_TOGGLED, goalId, date };
 }
 
 export function loadGoalsSuccess(goals) {
-    return { type: types.LOAD_GOALS_SUCCESS, goals};
+  return { type: types.LOAD_GOALS_SUCCESS, goals };
 }
 
 export function loadGoals() {
-    return function(dispatch) {
-        return goalApi.getAllGoals().then( goals => {
-            dispatch(loadGoalsSuccess(goals));
-        }).catch(error => {
-            throw(error);
-        });
-    }
+  return function(dispatch) {
+    return getAllGoals()
+      .then(goals => {
+        dispatch(loadGoalsSuccess(goals));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
 }
 
 export function saveGoal(goal) {
-    return function (dispatch, getState) {
-        return goalApi.saveGoal(goal).then(savedGoal => {
-            goal.id ? dispatch(updateGoalSuccess(savedGoal)) :
-                dispatch(createGoalSuccess(savedGoal));
-        }).catch(error => {
-            throw(error);
-        });
-    };
+  if (!goal.done) {
+    goal.done = false;
+  }
+  if (!goal.dates_done) {
+    goal.dates_done = [];
+  }
+  return function(dispatch, getState) {
+    return saveNewGoal(goal)
+      .then(savedGoal => {
+        goal.id
+          ? dispatch(updateGoalSuccess(savedGoal))
+          : dispatch(createGoalSuccess(savedGoal));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
 }
---
+++ src/api/GoalApi.js
@@ -0,0 +1,69 @@
+import delay from "./delay";
+import getBaseUrl from "./baseUrl";
+
+// This file mocks a web API by working with the hard-coded data below.
+// It uses setTimeout to simulate the delay of an AJAX call.
+// All calls return promises.
+
+const baseUrl = getBaseUrl();
+
+const goals = [
+  {
+    id: "brush-my-teeth",
+    name: "Brush my teeth",
+    done: false,
+    dates_done: ["2019-03-01", "2019-03-02"]
+  },
+  {
+    id: "sit-with-my-feelings",
+    name: "Sit with my feelings",
+    done: false,
+    dates_done: []
+  },
+  {
+    id: "look-out-of-the-window",
+    name: "Look out of the window",
+    done: false,
+    dates_done: ["2019-02-01", "2019-02-02", "2019-02-03", "2019-02-04"]
+  }
+];
+
+export function getAllGoals() {
+  return get("goals");
+}
+
+export function saveNewGoal(goal) {
+  return post("goals", goal);
+}
+
+function get(url) {
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function post(url, data) {
+  return fetch(baseUrl + url, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  }).then(onSuccess, onError);
+}
+
+export function deleteGoal(id) {
+  return del(`goals/${id}`);
+}
+
+function del(url) {
+  return fetch(baseUrl + url, {
+    method: "delete"
+  }).then(onSuccess, onError);
+}
+
+function onSuccess(response) {
+  return response.json();
+}
+
+function onError(error) {
+  console.log(error);
+}
