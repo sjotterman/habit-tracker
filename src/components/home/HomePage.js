82320ceb7255631c18f0d228173d973cb3d8e28a
Samuel Otterman <sjotterman@gmail.com>
2019-03-19 22:06:56 -0400

    edit home page text

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,15 +1,17 @@
-import React from 'react';
-
+import React from "react";
 
 class HomePage extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Home Page</h1>
-                <p>This doesn't do anything yet. Click 'Track' above to test out the tracker!</p>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <p>
+          This doesn't do anything yet. Click 'Goals' above to test out the
+          habit tracker!
+        </p>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
