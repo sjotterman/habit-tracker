d42ea41e11b599f2aaeda4483b3dfad726b69e2f
Samuel Otterman <sjotterman@gmail.com>
2019-03-30 22:03:09 -0400

    Add spinner to manage goals page

--- src/components/manageGoals/ManageGoalsPage.js
+++ src/components/manageGoals/ManageGoalsPage.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as goalsActions from "../../actions/goalsActions";
 import ManageGoalList from "./ManageGoalList";
+import Spinner from "../shared/Spinner";
 import { toast } from "react-toastify";
 
 class ManageGoalsPage extends React.Component {
@@ -23,10 +24,17 @@ class ManageGoalsPage extends React.Component {
 
   handleSubmit(event) {
     this.setState({ saving: true, fieldValue: "" });
-    this.props.actions.createGoal({ name: this.state.fieldValue }).then(() => {
-      toast.success("Goal created!");
-      this.setState({ saving: false });
-    });
+    this.props.actions
+      .createGoal({ name: this.state.fieldValue })
+      .then(() => {
+        toast.success("Goal created!");
+        this.setState({ saving: false });
+      })
+      .catch(error => {
+        this.setState({ saving: false });
+        // should set this error in a flash message
+        console.log(error);
+      });
     event.preventDefault();
   }
 
@@ -41,15 +49,19 @@ class ManageGoalsPage extends React.Component {
     let { goals } = this.props;
     return (
       <div>
-        <h1 className="text-light">Goals</h1>
-        <ManageGoalList
-          goals={goals}
-          onGoalDelete={this.handleDelete}
-          handleChange={this.handleChange}
-          fieldValue={this.state.fieldValue}
-          handleSubmit={this.handleSubmit}
-          saving={this.state.saving}
-        />
+        <h1 className="text-light">Manage Goals</h1>
+        {this.props.loading ? (
+          <Spinner />
+        ) : (
+          <ManageGoalList
+            goals={goals}
+            onGoalDelete={this.handleDelete}
+            handleChange={this.handleChange}
+            fieldValue={this.state.fieldValue}
+            handleSubmit={this.handleSubmit}
+            saving={this.state.saving}
+          />
+        )}
       </div>
     );
   }
@@ -57,7 +69,8 @@ class ManageGoalsPage extends React.Component {
 
 function mapStateToProps(state, ownProps) {
   return {
-    goals: state.goals
+    goals: state.goals,
+    loading: state.apiCallsInProgress > 0
   };
 }
 
