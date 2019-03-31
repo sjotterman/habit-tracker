6f12de4802d7da8eef452dee179608982c2c4ade
Samuel Otterman <sjotterman@gmail.com>
2019-03-31 17:06:37 -0400

    fix button color on goal complete

--- src/components/goals/GoalRow.js
+++ src/components/goals/GoalRow.js
@@ -24,7 +24,7 @@ const GoalRow = props => {
       <td>
         <div
           className={
-            "btn btn-goal-big " + (goal.done ? "btn-goal-done" : "btn-goal")
+            "btn btn-goal-big " + (doneToday ? "btn-goal-done" : "btn-goal")
           }
           onClick={props.onGoalToggle}
         >
