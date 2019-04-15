f248757f697ee89156136f3b85ec6b8d5a70a647
Samuel Otterman <sjotterman@gmail.com>
2019-04-14 22:56:31 -0400

    welcome message on home page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,14 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      profile: null,
+      error: ""
+    };
+  }
+  componentDidMount() {
+    this.loadUserProfile();
+  }
+
+  loadUserProfile() {
+    this.props.auth.getProfile((profile, error) => {
+      this.setState({ profile, error });
+    });
+  }
   render() {
-    const { isAuthenticated, login, getProfile } = this.props.auth;
+    const { isAuthenticated, login } = this.props.auth;
+    const { profile } = this.state;
+    let displayName;
+    let welcomeText;
+    if (profile) {
+      displayName = profile.given_name || profile.nickname;
+      welcomeText = profile ? `Welcome, ${displayName}!` : "Welcome!";
+    }
     return (
       <div>
         <h1>Home Page</h1>
         {isAuthenticated() ? (
           <>
-            <p>Welcome!</p>
+            <p>{welcomeText}</p>
+
             <Link to="/goals" className="nav-link">
               <button className="btn btn-primary">See my goals</button>
             </Link>
